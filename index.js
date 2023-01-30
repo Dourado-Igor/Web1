8b8752948bccd0b70b8e45fabb6e4d96fb8cb1b9
Dourado1983 <igorgonzalezdourado@gmail.com>
2023-01-30 18:59:44 +0100

    web1

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require('express')
-const path = require(`path`);
-var bodyparser = require("body-parser");
+const path =require('path');
+var mysql=require("mysql");
+var bodyparser=require("body-parser");
 const app = express()
 const port = 3000
 
-app.use (bodyparser.urlencoded{(extended:false)})
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    let options = rootpath.join (_dirname);
-
+let options={
+    root:path.join(__dirname)
+}
   res.sendFile("/views/index.html",options);
 })
-app.post("/",(req,res) => {
-console.log(req.body);
-res.send ("recibiendo datos..");
 
+
+app.post("/",(req,res)=>{
+    var conn=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'biblioteca'
+    });
+    conn.connect();
+    var sql=`insert into libros
+     (codigo,titulo,genero)
+     values ('${req.body.codigo}','${req.body.titulo}','${req.body.genero}')`;
+    conn.query(sql,(err,result,field)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(result)
+        res.send("recibiendo datos..")
+        conn.end();
+    })
+     
+    
 })
-app.get(`/hola`,(req,res) =>{
-   res.send(`Bien-Venido`);
+
+
+app.get("/hola",(req,res)=>{
+res.send("hola que tal");
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
